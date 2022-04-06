be6cb1841d638fac1d5f180714503217d6b7e062
jlshix <jlshix@163.com>
2022-04-06 22:51:44 +0800

    feat: 文书基类

---
+++ document_parser/datastructures/document_base.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+"""文书基类
+"""
+
+from pydantic import BaseModel
+
+from .typing import StrSet
+
+
+class DocumentBase(BaseModel):
+    """文书基类"""
+
+    #: 文书编号
+    id: str
+
+    #: 文书标题
+    title: str
+
+    #: 解析类型
+    types: StrSet
